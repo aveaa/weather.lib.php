dc61e1edfe1b1b66462a059749dcb428b35aeef5
zziger <zziger07@gmail.com>
2018-02-21 09:49:57 +0200

    в$+("($$($(-

--- bot.js
+++ bot.js
@@ -68,7 +68,7 @@ client.on("messageDelete", message => {
 
 client.on('ready', () => {
 	console.log('Bot loaded');
-	client.user.setPresence({ game: { name: `на тебя о_О`, type: 3 } });
+	client.user.setPresence({ game: { name: `тебя внимательно`, type: 4 } });
 })
 
 
