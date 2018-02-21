1e17559702ff63d2f94a800b95e9642583526727
zziger <zziger07@gmail.com>
2018-02-21 16:38:32 +0200

    hmm

--- bot.js
+++ bot.js
@@ -227,11 +227,11 @@ client.on("message", message => {
 		if (parseInt(args[0]) > Math.ceil(cmds.length/5) || parseInt(args[0]) < 1 || args.length == 0) page = 1;
 		var cmds_list = cmds.slice(1+((page-1)*5), 6+((page-1)*5));
 		const embed = new Discord.RichEmbed()
-		.setTitle(`Помощь пользователя ${message.author.tag}`)
+		.setTitle(`Помощь пользователя ${message.author}`)
 		.setFooter(`Страница ${page}/${Math.ceil(cmds.length/5)}`)
 		.setColor(parseInt(getRandomInt(0,16777214)))
 		.setDescription(cmds_list.join('\n'));
-		message.reply({embed});
+		message.channel.send({embed});
 		message.delete();
 
 	} else if (command=='roles' || command == 'роли') {
